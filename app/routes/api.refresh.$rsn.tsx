19e9093f76824843f8c360c5b72d16a42413ddee
jay <jay@jeepies.codes>
2025-07-05 20:46:34 +0100

    delete: fully wipe all current methods

--- app/routes/api.refresh.$rsn.tsx
+++ app/routes/api.refresh.$rsn.tsx
@@ -1,16 +1,16 @@
-import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
-import { getFreshestData } from '~/services/model/player.server';
+// import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+// import { getFreshestData } from '~/services/model/player.server';
 
-export async function loader({ params }: LoaderFunctionArgs) {
-  const rsn = params.rsn?.toLowerCase().trim();
-  if (!rsn) throw new Response('Missing RSN', { status: 400 });
-  await getFreshestData(rsn);
-  return { success: 'idk' };
-}
+// export async function loader({ params }: LoaderFunctionArgs) {
+//   const rsn = params.rsn?.toLowerCase().trim();
+//   if (!rsn) throw new Response('Missing RSN', { status: 400 });
+//   await getFreshestData(rsn);
+//   return { success: 'idk' };
+// }
 
-export async function action({ params }: ActionFunctionArgs) {
-  const rsn = params.rsn?.toLowerCase().trim();
-  if (!rsn) throw new Response('Missing RSN', { status: 400 });
-  getFreshestData(rsn);
-  return { success: 'idk' };
-}
+// export async function action({ params }: ActionFunctionArgs) {
+//   const rsn = params.rsn?.toLowerCase().trim();
+//   if (!rsn) throw new Response('Missing RSN', { status: 400 });
+//   getFreshestData(rsn);
+//   return { success: 'idk' };
+// }
