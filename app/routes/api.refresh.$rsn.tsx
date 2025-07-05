0edeb956b939be5408cab2439fbf1a6f297d5943
jay <jay@jeepies.codes>
2025-07-05 23:47:45 +0100

    feat(api): implement loader and action to refresh player data
    Uncomment loader and action functions to enable fetching the freshest
    player data by RSN. Normalize RSN input and handle missing RSN with a
    400 response. Return success status based on the data retrieval result.

--- app/routes/api.refresh.$rsn.tsx
+++ app/routes/api.refresh.$rsn.tsx
@@ -1,16 +1,18 @@
-// import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
-// import { getFreshestData } from '~/services/model/player.server';
+import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { getFreshestData } from '~/~models/player.server';
 
-// export async function loader({ params }: LoaderFunctionArgs) {
-//   const rsn = params.rsn?.toLowerCase().trim();
-//   if (!rsn) throw new Response('Missing RSN', { status: 400 });
-//   await getFreshestData(rsn);
-//   return { success: 'idk' };
-// }
+export async function loader({ params }: LoaderFunctionArgs) {
+  const rsn = params.rsn?.toLowerCase().trim();
+  if (!rsn) throw new Response('Missing RSN', { status: 400 });
+  const data = await getFreshestData(rsn);
+  if (typeof data === 'string') return { success: false };
+  return { success: true };
+}
 
-// export async function action({ params }: ActionFunctionArgs) {
-//   const rsn = params.rsn?.toLowerCase().trim();
-//   if (!rsn) throw new Response('Missing RSN', { status: 400 });
-//   getFreshestData(rsn);
-//   return { success: 'idk' };
-// }
+export async function action({ params }: ActionFunctionArgs) {
+  const rsn = params.rsn?.toLowerCase().trim();
+  if (!rsn) throw new Response('Missing RSN', { status: 400 });
+  const data = await getFreshestData(rsn);
+  if (typeof data === 'string') return { success: false };
+  return { success: true };
+}
