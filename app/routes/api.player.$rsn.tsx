072bc24c2c4165a4f5a12ab851e4c4d4cfb647d2
jay <jay@jeepies.codes>
2025-07-19 16:22:35 +0100

    feat(api): implement player data loader with existence check
    Add loader function to fetch player data by RSN with validation.  
    Default to 'Kelcei' if no RSN is provided. Check if the player exists  
    using Runescape service before fetching data. Return appropriate  
    error messages for non-existent users and internal errors. Improve  
    error handling and response consistency in the API endpoint.

--- app/routes/api.player.$rsn.tsx
+++ app/routes/api.player.$rsn.tsx
@@ -1,11 +1,35 @@
-import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { cli } from '@remix-run/dev';
+import { LoaderFunctionArgs } from '@remix-run/node';
 import { MethodNotAllowed } from '~/~constants/API';
+import { PlayerDataFetcher } from '~/~models/data-fetcher.server';
+import { Runescape } from '~/~services/runescape.server';
 import { APIResponse } from '~/~types/API';
 
 export async function loader({ params }: LoaderFunctionArgs): Promise<APIResponse> {
-  const response: APIResponse = { success: false, error: 'Method not yet implemented' };
+  const response: APIResponse = { success: false, error: 'Unexpected server error' };
+  let rsn = params.rsn?.toLowerCase().trim();
+  if (!rsn) rsn = 'Kelcei';
 
-  return response;
+  const exists = await Runescape.checkExistence([rsn]);
+  console.log(exists);
+  if (!exists?.get(rsn)) {
+    response.error = 'This user does not exist';
+    return response;
+  }
+
+  try {
+    const client = await PlayerDataFetcher.instance(rsn);
+    if (!client) {
+      response.error = 'An internal server error occured. Please try again';
+      return response;
+    }
+    const data = await client?.getFreshestData();
+
+    return { success: true, data: data };
+  } catch {
+    response.error = 'An internal server error occured. Please try again';
+    return response;
+  }
 }
 
 export async function action(): Promise<APIResponse> {
